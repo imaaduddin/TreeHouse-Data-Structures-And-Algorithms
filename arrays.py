370fbec1d0cb4e66ac9fc63fc78ba41b2fa5ee6d
Imaad Uddin <imaaduddin@icloud.com>
2021-08-12 22:00:04 -0500

    linked lists

--- arrays.py
+++ arrays.py
@@ -7,3 +7,17 @@ if 1 in new_list:
 for n in new_list:
   if n == 1:
     print(True)
+
+    break
+
+numbers = []
+len(numbers)
+
+numbers.append(2)
+numbers.append(200)
+
+letters = []
+letters.extend(["a", "b", "c"])
+
+
+
