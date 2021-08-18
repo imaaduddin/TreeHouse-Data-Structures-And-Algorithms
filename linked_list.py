a6ed80e5e303a5b11daecd693c844475ea3b0f09
Imaad Uddin <imaaduddin@icloud.com>
2021-08-17 21:21:09 -0500

    adding node

--- linked_list.py
+++ linked_list.py
@@ -29,6 +29,13 @@ class LinkedList:
       count = count + 1
       current = current.next_node
     return count
+
+    def add(self, data):
+      # Adds new Node containing data at head of the list 
+      # Takes O(1) time
+      new_node = Node(data)
+      new_node.next_node = self.head
+      self.head = new_node
       
       
 
