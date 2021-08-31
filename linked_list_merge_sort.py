6cdd70dd5d1b317e30543e7290674d331492a70f
Imaad Uddin <imaaduddin@icloud.com>
2021-08-30 19:03:01 -0500

    merge sort

--- linked_list_merge_sort.py
+++ linked_list_merge_sort.py
@@ -22,3 +22,19 @@ def split(linked_list):
   # Divide the unsorted list at midpoint into sublists 
   if linked_list == None or linked_list.head == None:
     left_half = linked_list
+    right_half = None 
+
+    return left_half, right_half
+  else:
+    size = linked_list.size()
+    mid = size // 2
+
+    mid_node = linked_list.node_at_index(mid - 1)
+
+    left_half = linked_list
+    right_half = LinkedList()
+    right_half.head = mid_node.next_node
+    mid_node.next_node = None
+
+    return left_half, right_half
+    
