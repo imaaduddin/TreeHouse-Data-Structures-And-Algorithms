dd7438a4c2fc36cf457b1e917e5c7c235354dbd6
Imaad Uddin <imaaduddin@icloud.com>
2021-09-03 19:21:49 -0500

    recursion

---
+++ recursion.py
@@ -0,0 +1,15 @@
+# def sum(numbers):
+#   total = 0
+#   for number in numbers:
+#     total+= number
+#   return total
+# print(sum([1, 2, 3, 4, 5]))
+
+# Recursive Function
+def sum(numbers):
+  if not numbers:
+    return 0
+  remaining_sum = sum(numbers[1:])
+  return numbers[0] + remaining_sum
+
+print(sum([1, 2, 7, 9]))
